c9e76203e98af4e588984c83ef6583382de58b9a
Cowcat5150 <lilgodot2@gmail.com>
2021-01-05 18:32:05 +0100

    Homebrew Morphos makefile.

--- config.h
+++ config.h
@@ -1,6 +1,22 @@
 #ifndef __DOOM3_CONFIG_H__
 #define __DOOM3_CONFIG_H__
 
+#if defined(__MORPHOS__)
+
+#define BUILD_OS		"morphos"
+#define BUILD_CPU		"ppc"
+
+#define BUILD_LIBRARY_SUFFIX	".so"
+
+#define BUILD_LIBDIR		""
+#define BUILD_DATADIR		""
+
+/* #undef HAVE_JPEG_MEM_SRC */
+
+/* #define ID_ENABLE_CURL */
+
+#else
+
 #define BUILD_OS				"linux"
 #define BUILD_CPU				"x86_64"
 
@@ -12,5 +28,7 @@
 /* #undef HAVE_JPEG_MEM_SRC */
 
 /* #undef ID_ENABLE_CURL */
+
+#endif
 
 #endif
