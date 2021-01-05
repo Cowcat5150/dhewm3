bfecda9adeb72690edb0177b8b6c14576c4441f0
Cowcat5150 <lilgodot2@gmail.com>
2021-01-05 13:34:33 +0100

    Homebrew makefile.

---
+++ config.h
@@ -0,0 +1,16 @@
+#ifndef __DOOM3_CONFIG_H__
+#define __DOOM3_CONFIG_H__
+
+#define BUILD_OS				"linux"
+#define BUILD_CPU				"x86_64"
+
+#define BUILD_LIBRARY_SUFFIX	".so"
+
+#define BUILD_LIBDIR			"/usr/local/lib/dhewm3"
+#define BUILD_DATADIR			"/usr/local/share/dhewm3"
+
+/* #undef HAVE_JPEG_MEM_SRC */
+
+/* #undef ID_ENABLE_CURL */
+
+#endif
